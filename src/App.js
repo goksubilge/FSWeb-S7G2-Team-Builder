91d841fad4c5e3e0646489884604882a97ee9795
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-07-11 22:28:38 +0300

    display members on the screen

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import { useState } from 'react';
 
 function App() {
 const [memberList, setMemberList ] = useState ([
-    "member1","member2","member 3"
+    {name: "Elaine Benes", category:"main", role: "assistant" },
+    {name: "George Costanza", category:"main",role: "lawyer" },
+    {name: "Cosmo Kramer", category:"main", role: "unemployled"},
+    {name: "Newman", category:"main", role: "postman" },
+    {name: "Jerry Seinfeld", category:"main", role: "comedian" },
+    {name: "Susan Ross", category:"support", role: "Costanza's fiancee" },
+    {name: "Leslie", category:"support", role: "Kramer's GF" },
   ])
 
 
@@ -14,8 +20,12 @@ const [memberList, setMemberList ] = useState ([
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> WIT STARTING </p>
-        {memberList.map((member) => ( <p>{member}</p> ))}
+        <h1> 'Seinfeld' characters application form </h1>
+        {memberList.map((member, i) => (
+          <p key={i}>
+            {member.name} - {member.category}- {member.role}
+          </p>
+        ))}
       </header>
     </div>
   );
