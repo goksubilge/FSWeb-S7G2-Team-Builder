f26415c1db3066605487f7494b8825fc158bcb55
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-07-11 23:56:17 +0300

    change the image,css + add form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useState } from 'react';
-
 
 
 function App() {
@@ -14,18 +13,36 @@ const [memberList, setMemberList ] = useState ([
     {name: "Susan Ross", category:"support", role: "Costanza's fiancee" },
     {name: "Leslie", category:"support", role: "Kramer's GF" },
   ])
-
+  // <form> a onsubmit ekleyip for olan html leri de js e göre htmlFor olarak düzenledim. tıpkı className gibi. yukarı da bir submitFN yaptım preventDefault lu.
+  // label ları strong etiketiyle değiştirdim, biraz kalın etiket olsunlar diye. inputlarda id ='fname' vardı onları kaldırdım burda i ile tanımladım ihtiyacım yok artık ordakilere.
+  // <input type="submit" value="Submit"/> bunu button ile değiştirdim çünkü buttonda içine onClick yazmama gerek kalmıyor.
+  // button un type ına submit dediğim için gidip submite dair bişey aradı ve submit fonksiyonunu buldu. onu çalıştıracak. ya da buttonun içine onClick={submitFN} yazarsam gerek yok form un içine yazmama.
+  //
+  
+  const submitFN= (e) => {
+    //console.log("submitfonks çalıştırma kontrolü");
+    e.preventDefault();
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1> 'Seinfeld' characters application form </h1>
         {memberList.map((member, i) => (
-          <p key={i}>
+          <span key={i}>
             {member.name} - {member.category}- {member.role}
-          </p>
+          </span>
         ))}
+
+        <form onSubmit={submitFN}>
+          <strong htmlFor="name">name:</strong>
+          <input type="text" name="name" value="John"/><br></br>
+          <strong htmlFor="category">category:</strong>
+          <input type="text" name="category" value="Doe"/><br></br>
+          <strong htmlFor="role">role:</strong>
+          <input type="text" name="role" value="Doe"/><br></br>
+          <button type="submit">.   SEND   .</button>
+        </form>
       </header>
     </div>
   );
